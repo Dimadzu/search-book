52d07f32327db11370ee6fae99d0a8d3f371db10
Dimadzu <Dimadzu@i.ua>
2021-06-23 21:52:41 +0300

    init: Initial project version

---
+++ src/Api.js
@@ -0,0 +1,2 @@
+export const API_URL_SERCH="https://www.googleapis.com/books/v1/volumes?q";
+export const API_KEY="AIzaSyAJyyY7S0PxMjNTnGhCYl2b-HjG4ulbDPc";
--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { API_KEY, API_URL_SERCH } from './Api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      
+
+    }
+  };
+  render() {
+    const search="mobi dik";
+    async function  searchBook(){
+      const response=await fetch(`${API_URL_SERCH}=${search}:keyes&key=${API_KEY}` );
+      const data = await response.json();
+      console.log(data);
+   }
+   searchBook()
+    return (
+      <div className="App">
+        <header className="App-header">
+
+
+
+
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
