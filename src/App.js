fcd121cfbe0f26866d9a268e0af463d903b3e613
Dimadzu <dimadzu@i.ua>
2021-06-24 14:35:13 +0300

    feat: add function search to click button

--- src/App.js
+++ src/App.js
@@ -3,27 +3,59 @@ import './App.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { API_KEY, API_URL_SERCH } from './Api';
+import 'antd/dist/antd.css';
+import { Input, Space } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
 
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      
+      onSearch:''
 
     }
   };
+   getData= async function searchBook() {
+    const response = await fetch(`${API_URL_SERCH}=${this.state.onSearch}:keyes&key=${API_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
+  componentDidMount(){
+
+  };
+  componentDidUpdate(prevProps,prevState) {
+    if(prevState.onSearch!==this.state.onSearch) this.getData();
+  };
+
   render() {
-    const search="mobi dik";
-    async function  searchBook(){
-      const response=await fetch(`${API_URL_SERCH}=${search}:keyes&key=${API_KEY}` );
-      const data = await response.json();
-      console.log(data);
-   }
-   searchBook()
+    const onSearch = value =>this.setState({onSearch: value.toUpperCase()});
+
+    const { Search } = Input;
+    const suffix = (
+      <AudioOutlined
+        style={{
+          fontSize: 16,
+          color: '#1890ff',
+        }}
+      />
+    );
+
+
+
     return (
       <div className="App">
         <header className="App-header">
+          <Space direction="vertical">
+            <Search
+              placeholder="input search text"
+              enterButton="Search"
+              size="large"
+              suffix={suffix}
+              onSearch={onSearch}
+              
+            />
+          </Space>
 
 
 
